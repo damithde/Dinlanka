e4a99ccfcac87ecaa8102c3c082df5ece2a2e544
Shermi <shermiliyanaarachchi@gmail.com>
2019-06-16 17:06:01 +0530

    ocean freight page
    ocean freight page

--- dinlanka-system/src/components/home/OceanFreight.js
+++ dinlanka-system/src/components/home/OceanFreight.js
@@ -23,6 +23,86 @@ class OceanFreight extends Component {
         
         return (
         <React.Fragment>
+         <header className="site-navbar py-3" role="banner">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-11 col-xl-2">
+              <h1 className="mb-0"><a href="index.html" className="text-white h2 mb-0">DinLanka</a></h1>
+            </div>
+            <div className="col-12 col-md-10 d-none d-xl-block">
+              <nav className="site-navigation position-relative text-right" role="navigation">
+                <ul className="site-menu js-clone-nav mx-auto d-none d-lg-block">
+                  <li className="active"><a href="index.html">HOME</a></li>
+                  <li><a href="/aboutus">ABOUT US</a></li>
+                  <li className="has-children">
+                    <a href="services.html">KEY SERVICES</a>
+                    <ul className="dropdown">
+                      <li><a href="/iorservice">IOR/EOR Services</a></li>
+                      <li><a href="/oceanfreight">Ocean Freight</a></li>
+                      <li><a href="/airfreight">Air Freight</a></li>
+                      <li><a href="/seaairservice">Sea Air Service</a></li>
+                      <li><a href="/cargoinsurance">Cargo Insurance</a></li>
+                      <li><a href="/containers">New and Used Containers</a></li>
+                      <li><a href="/supplychain">Supply Chain Management</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="/upbcargo">UPB CARGO</a></li>
+                  <li className="has-children">
+                    <a href="services.html">CUSTOMERS</a>
+                    <ul className="dropdown">
+                      <li><a href="/supportcenter">Support Center</a></li>
+                      <li><a href="/tracking">Dinlanka Tracking</a></li>
+                      <li><a href="/needquote">Need A Quote</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="/contact">CONTACT US</a></li>
+                  <li><a href="/signin">LOGIN</a></li>
+                  
+                </ul>
+              </nav>
+            </div>
+            <div className="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style={{position: 'relative', top: '3px'}}><a href="#" className="site-menu-toggle js-menu-toggle text-white"><span className="icon-menu h3" /></a></div>
+          </div>
+        </div>
+      </header>
+      <div className="site-blocks-cover overlay" style={{backgroundImage: "url(" + bg + ")"}} data-aos="fade" data-stellar-background-ratio="0.5">
+        <div className="container">
+          <div className="row align-items-center justify-content-center text-center">
+            <div className="col-md-8" data-aos="fade-up" data-aos-delay={400}>
+            
+              <h1 className="text-white font-weight-light mb-5 font-weight-bold">DinLanka Logistics (Pvt) Ltd</h1>
+              <h3 className="text-white font-weight-light mb-5 font-weight">“SERVICE BEYOND BORDERS” ONE OF THE BEST TOTAL LOGISTICS PROVIDER</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="site-section">
+      <div className="container">
+        <div className="row">
+        <div class="col-md-10 col-lg-10 mb-10 ">
+            <div class="meta mb-4">
+              <h2 className="font-size-regular"><a href="#">Ocean Freight </a></h2>
+              <p>The Dinlanka Forwarding network provides a complete range of sea freight services to meet both your import and export requirements. Our market competitive offering delivers shipment, order and product line visibility via Dinlanka’s information control platform.</p>
+              <p>The suite of sea freight services outlined below are provided in a choice of transit time options:</p>
+              <ul>
+              <li>20, 40 and 45 foot general purpose and high cube Full Container Loads (FCL)</li>
+              <li>Operating and non-operating reefer containers</li>
+              <li>Special container equipment</li>
+              <li>Less than Container Load (LCL)</li>
+              <li>Break bulk cargo/non-containerised loads</li>
+             </ul>
+              <p>In addition to the sea service options Dinlanka Forwarding offers the following sea service expertise:</p>
+              <ul>
+              <li>Project Services</li>
+              <li>Ship charter</li>
+              <li>Special handling</li>
+             </ul>
+            </div>
+          </div>          
+        </div>
+      </div>
+    </div>
+
           
          
         </React.Fragment>
