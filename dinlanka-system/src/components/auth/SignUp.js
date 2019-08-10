82056f67ecf804c9e118611e0cb5d6c63afa441e
Shermi <shermiliyanaarachchi@gmail.com>
2019-08-10 11:33:26 +0530

    sign up page
    sign up page

--- dinlanka-system/src/components/auth/SignUp.js
+++ dinlanka-system/src/components/auth/SignUp.js
@@ -3,14 +3,14 @@ import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { signUp } from '../../store/actions/authActions'
 import { Button, Card, CardBody, CardGroup, Col,CustomInput,FormGroup, Container,Modal,ModalHeader,ModalBody,ModalFooter, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
-import {TextInput,NavItem,CardPanel,Select}from 'react-materialize';
-
+import { Label, FormFeedback, FormText , CardFooter,CardHeader,} from 'reactstrap';
+import AdminNavbar from '../layouts/AdminNavbar';
 
 class SignUp extends Component {
   state = {
     email: '',
     password: '',
-    fName: '',
+    fullName: '',
     username: '',
     isAdmin:'',
   }
@@ -25,62 +25,95 @@ class SignUp extends Component {
   }
   render() {
     const { auth, authError } = this.props;
-    if (auth.uid) return <Redirect to='/' /> 
-    return (     
-        <div className="app align-items-center">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md="8">
-              <CardGroup>
-                <Card className="p-4">
-                  <CardBody style={{ color:'black' }}>
-                    <Form onSubmit={this.handleSubmit}>
-                    <CardPanel className="teal">
-                        <span >
-                        <div className="text-center" style={{color: 'black'}}><h1>REGISTER USERS</h1></div>
-                        </span>
-                    </CardPanel>
-                      <InputGroup className="mb-3">
-                        <TextInput  icon="face" type="text" placeholder="Full Name"  id= 'fName'  onChange={this.handleChange} />
-                      </InputGroup>
-                      <InputGroup className="mb-3">
-                        <TextInput  icon="face" type="text" placeholder="Username"  id= 'username'  onChange={this.handleChange} />
-                      </InputGroup>
-                      <InputGroup className="mb-3">
-                        <TextInput  icon="email" type="email" placeholder="Email" id= 'email' onChange={this.handleChange} />
-                      </InputGroup>
-                      <InputGroup className="mb-3">
-                        <Select icon="cloud" id='isAdmin'  onChange={this.handleChange}>
-                            <option value="Admin" >
-                                Admin
-                            </option>
-                            <option value="Agent">
-                                Agent
-                            </option>
-                        </Select>
-                      </InputGroup>
-                      <InputGroup className="mb-4">
-                        <TextInput  icon="lock" type="password" placeholder="Password" id= 'password' onChange={this.handleChange} />
-                      </InputGroup>                
-                      <Row >
-                          <Col xs={{ size: 7.5, offset: 5 }} >
-                          <div className="text-right"><Button   color="dark" className="text-center">Submit</Button></div>
-                          </Col>
-                      </Row>
-                    </Form>
-                  </CardBody>
-                </Card>         
-              </CardGroup>
-            </Col>
-          </Row>
-        </Container>
-        <div className="center red-text">
-        { authError ? <p>{authError}</p> : null }
-        </div>
+    //if(!auth.uid){return <Redirect to='/signin' />}
+    
+    return ( 
+     
+      <React.Fragment>
+          <AdminNavbar/> 
+          <div className="container">
+ 
+          <Card>
+              <CardHeader className='bg-primary'>
+                <strong><h3>Regsiter Agents</h3></strong> 
+              </CardHeader>
+              <CardBody>
+                <Form id="tex" onSubmit={this.handleSubmit} className="form-horizontal">
+                  <FormGroup row>
+                    <Col md="3">
+                      
+                    </Col>
+                    <Col xs="12" md="9">
+                      <p className="form-control-static"></p>
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Col md="2">
+                      <Label htmlFor="text-input">Full Name</Label>
+                    </Col>
+                    <Col xs="12" md="9">
+                      <Input type="text" id='fullname' onChange={this.handleChange} name="text-input" placeholder="Full name" />
+                      
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Col md="2">
+                      <Label htmlFor="text-input">User Name</Label>
+                    </Col>
+                    <Col xs="12" md="9">
+                      <Input type="text" id="username" name="text-input" placeholder="username" onChange={this.handleChange} />
+                      
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Col md="2">
+                      <Label htmlFor="text-input">User Type</Label>
+                    </Col>
+                    <Col xs="12" md="9">
+                    <Input type="select" name="isAdmin" id="isAdmin" onChange={this.handleChange} >
+                            <option value="yes"> Admin</option>
+                            <option value="no">Agent</option>
+                    </Input>                     
+                    </Col>
+                  </FormGroup>
+
+                  <FormGroup row>
+                    <Col md="2">
+                      <Label htmlFor="email-input">Email</Label>
+                    </Col>
+                    <Col xs="12" md="9">
+                      <Input type="text" id="email" name="text-input" placeholder="email" onChange={this.handleChange} />
+                     
+                    </Col>
+                  </FormGroup>
+                  <FormGroup row>
+                    <Col md="2">
+                      <Label htmlFor="text-input">Password</Label>
+                    </Col>
+                    <Col xs="12" md="9">
+                      <Input type="password" id="password" name="text-input" placeholder="password" onChange={this.handleChange} />                    
+                    </Col>
+                  </FormGroup>
+                                 
+              
+              <CardFooter>
+                <Button form="tex" type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>
+                <Button form="tex" type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
+              </CardFooter>
+              </Form>
+              </CardBody>
+
+           </Card>
+           <div className="center red-text">
+              { authError ? <p>{authError}</p> : null }
+            </div>
+
       </div>
+      </React.Fragment>
     );
   }
 }
+
 
 const mapStateToProps = (state) => {
   return {
--- dinlanka-system/src/components/layouts/AdminNavbar.js
+++ dinlanka-system/src/components/layouts/AdminNavbar.js
@@ -1,26 +1,43 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { signOut } from '../../store/actions/authActions'
 
 
-const AdminNavbar = () => {
+const AdminNavbar = (props) => {
     return(
-        <div>
-      <ul id="dropdown1" className="dropdown-content" >
-      <li><NavLink to='/'>Register New Agent</NavLink></li>
-            <li><NavLink to='/'>Edit</NavLink></li>
-            </ul>
-            <ul className="right">
-        <li><NavLink to='/'>Agents</NavLink></li>
-            
+      <nav className="navbar navbar-expand-lg  bg-dark">
         
-        <li><NavLink to='/'>Login</NavLink></li>
-        <li><a className="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i className="material-icons right">arrow_drop_down</i></a></li>
-
-      </ul>
-    </div>
+      <div className="collapse navbar-collapse" id="navbarColor01">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/signup">Register Agents</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Location Update</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">Employee Details</a>
+          </li>
+       
+        </ul>
+        <ul className="navbar-nav">
+        <li className="nav-item">
+        <a className="nav-link bg-primary" onClick={props.signOut}>Log Out</a>
+          </li>
+        </ul>
+     
+      </div>
+    </nav>
 
     )
 
 
 }
-export default AdminNavbar;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signOut: () => dispatch(signOut())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AdminNavbar)
